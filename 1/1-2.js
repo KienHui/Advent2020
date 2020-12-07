83367f3cfbb8277f3217035394f0eb22f9de20fa
KienHui <KienHui@users.noreply.github.com>
2020-12-07 05:20:09 -0600

    fixed null line issue

--- 1/1-2.js
+++ 1/1-2.js
@@ -4,19 +4,17 @@ fs.readFile("input",'utf8',(err,data) => {
         console.log(err);
     }
     splitdata = data.split("\n")
+    splitdata = splitdata.filter( arg => arg !=0 )
     out={};
     splitdata.forEach(p1 => {
-        if (p1 !=0) {
             splitdata.forEach(p2 => {            
-                if (p2 !=0) {
                     splitdata.forEach(p3 => {
-                        if (p3 !=0) {
                             out[p1/1+p2/1+p3/1] = p1*p2*p3;
-                        }
+                        
                     })
-                }
+                
             })
-        }
+        
     });
     console.log(out[2020]);
 })
